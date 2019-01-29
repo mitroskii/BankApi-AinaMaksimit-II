62451871414667049e02e7e20220eb7a4a890b99
TuomasVille <tuomas_kayhty@hotmail.com>
2019-01-29 10:36:58 +0200

    Added Models

---
+++ BankApp/BankApp/Model/Bank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankApp.Model
+{
+    public partial class Bank
+    {
+        public Bank()
+        {
+            Account = new HashSet<Account>();
+            Customer = new HashSet<Customer>();
+        }
+
+        public long Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string BIC { get; set; }
+
+        [InverseProperty("Bank")]
+        public virtual ICollection<Account> Account { get; set; }
+        [InverseProperty("Bank")]
+        public virtual ICollection<Customer> Customer { get; set; }
+    }
+}
